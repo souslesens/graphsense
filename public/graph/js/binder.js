b3008e347e2285175d28a3c3b60e8a364943ff55
Claude.fauconnet <claude.fauconnet@neuf.fr>
2019-03-22 18:08:26 +0100

    Initial commit

---
+++ public/graph/js/binder.js
@@ -0,0 +1,19 @@
+var binder=(function(){
+
+    self={}
+    self.bindOnPageload=function(){
+        var array=["circle","square"];
+        common.fillSelectOptionsWithStringArray("graphParamsDialog_shapeSelect",array,true);
+
+
+var xxx=  $("#filterCollapseMenu");
+
+        $("#filterCollapseMenu").load("htmlSnippets/queryFilters.html", function () {
+            alert("aa");
+        })
+
+
+    }
+
+   return self;
+})()
