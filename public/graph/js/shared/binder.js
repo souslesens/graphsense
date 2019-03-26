7047e06a895cc3295329d6575784c24a5396c274
jajacode <pierre.jallais@gmail.com>
2019-03-26 14:51:25 +0100

    Test master

--- public/graph/js/shared/binder.js
+++ public/graph/js/shared/binder.js
@@ -1,31 +1,40 @@
 var binder=(function(){
 
-    self={}
-    self.bindOnPageload=function(){
-        var array=["circle","square"];
-        common.fillSelectOptionsWithStringArray("graphParamsDialog_shapeSelect",array,true);
+  self={}
+  self.bindOnPageload=function() {
+    var array=["circle","square"];
+    common.fillSelectOptionsWithStringArray("graphParamsDialog_shapeSelect",array,true);
 
-        $("#filterCollapseMenu").load("htmlSnippets/queryFilters.html", function () {
-          console.log("queryFilters.html loaded")
-        })
+    $("#filterCollapseMenu").load("htmlSnippets/queryFilters.html", function () {
+      console.log("queryFilters.html loaded")
+    })
 
-        $("#expandCollapseMenu").load("htmlSnippets/queryExpand.html", function () {
-          console.log("queryExpand.html loaded")
-        })
+    $("#expandCollapseMenu").load("htmlSnippets/queryExpand.html", function () {
+      console.log("queryExpand.html loaded")
+    })
 
-        $("#highlightCollapseMenu").load("htmlSnippets/queryHighlight.html", function () {
-          console.log("queryHighlight.html loaded")
-        })
+    $("#highlightCollapseMenu").load("htmlSnippets/queryHighlight.html", function () {
+      console.log("queryHighlight.html loaded")
+    })
 
-      /*  var cypher = " match(n)-[]-(m) return n limit 100";
-        Cypher.executeCypher(cypher, function (err, result) {
-            if( err)
-                var xx=2
-        })*/
+    $("#displayModalContent").load("htmlSnippets/graphDisplayModal.html", function () {
+      console.log("graphDisplayModal.html loaded")
+    })
 
 
+  /*  var cypher = " match(n)-[]-(m) return n limit 100";
+    Cypher.executeCypher(cypher, function (err, result) {
+        if( err)
+            var xx=2
+    })*/
 
-    }
+
+    $("#myRange").on('input', function() {
+      console.log("myrange change input")
+      $("#myRangeTxt") = $("#myRange").value;
+    });
+
+  }
 
    return self;
 })()
