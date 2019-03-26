abbaf8e3db0760d3feba236fe2b1fdfb59e9b396
jajacode <pierre.jallais@gmail.com>
2019-03-26 22:59:33 +0100

    commit

--- public/graph/js/shared/binder.js
+++ public/graph/js/shared/binder.js
@@ -2,11 +2,29 @@ var binder=(function(){
 
   self={}
   self.bindOnPageload=function() {
-    var array=["circle","square"];
-    common.fillSelectOptionsWithStringArray("graphParamsDialog_shapeSelect",array,true);
 
-    $("#filterCollapseMenu").load("htmlSnippets/queryFilters.html", function () {
-      console.log("queryFilters.html loaded")
+    var array=["circle","square"];
+    common.fillSelectOptionsWithStringArray("graphParamsDialog_shapeSelect", array, true);
+
+
+    $("#dbFilterCollapseMenu").load("htmlSnippets/dbQueryFilter.html", function () {
+      console.log("queryFilters.html loaded");
+      var dbLabelToFilter = ["Label 1", "Label 2", "Label 3", "Label 4"];
+      var dbColorClassToFilter = ["primary", "secondary", "success", "danger", "warning","info","light","dark"];
+      var dbBadgeToFilter = [10,55,30,44,8,32];
+      
+      for(i=0;i<dbLabelToFilter.length;i++)
+      {
+        var buttonLabelToAdd = "<button type='button' class='btn btn-" + dbColorClassToFilter[i] + "' data-toggle='modal' data-target='#dbQueryFilterLabelModal'>" + dbLabelToFilter[i] + " <span class='badge badge-pill badge-light'>" + dbBadgeToFilter[i] + "</span></button>";
+        $("#dbFilterLabelButtonGroup").append( buttonLabelToAdd );
+      }
+
+      
+      $("#dbQueryFilterLabelModal").load("htmlSnippets/dbQueryFilterLabelModal.html", function () {
+        console.log("queryExpand.html loaded")
+      })
+      
+      
     })
 
     $("#expandCollapseMenu").load("htmlSnippets/queryExpand.html", function () {
@@ -19,6 +37,10 @@ var binder=(function(){
 
     $("#displayModalContent").load("htmlSnippets/graphDisplayModal.html", function () {
       console.log("graphDisplayModal.html loaded")
+      $("#myRange").on('input', function() {
+        $("#myRangeTxt").html( $(this).val() )
+
+      });
     })
 
 
@@ -29,10 +51,7 @@ var binder=(function(){
     })*/
 
 
-    $("#myRange").on('input', function() {
-      console.log("myrange change input")
-      $("#myRangeTxt") = $("#myRange").value;
-    });
+ 
 
   }
 
