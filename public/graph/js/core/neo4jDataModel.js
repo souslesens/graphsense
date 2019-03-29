43163defafdacae48a702778cf97a48d0cb125dd
Claude.fauconnet <claude.fauconnet@neuf.fr>
2019-03-29 18:34:34 +0100

    corrections

--- public/graph/js/core/neo4jDataModel.js
+++ public/graph/js/core/neo4jDataModel.js
@@ -100,11 +100,10 @@ var DataModel = (function () {
         )
 
 
-
     }
 
 
-    self.initNeoModel = function (subGraph, callback) {
+    self.initNeoModel = function (subGraph, callbackOuter) {
         self.labels = {};
         self.labelsRelations = {};
         self.relations = {};
@@ -112,216 +111,158 @@ var DataModel = (function () {
         self.allProperties = [""];
         self.allRelationsArray = [""];
         self.allLabels = [""];
-        var where = "";
-        if (subGraph && subGraph != "")
-            where = " where n.subGraph='" + subGraph + "'";
+        async.series([
+            function (callback) {
 
+                var where = "";
+                if (subGraph && subGraph != "") ;
+                where = " where n.subGraph='" + subGraph + "'";
+                var sql = "MATCH(n)-[r]-(m) "
+                    + where
+                    + " RETURN distinct labels(n) as labels_n, type(r) as type_r,labels(m)[0] as label_m, labels(startNode(r))[0] as label_startNode,count(n) as count_n,count(r) as count_r,count(m) as count_m";
+                Cypher.executeCypher(sql, function (err, data) {
+                    if (err)
+                        return callback(err);
+                    if (data.length == 0)
+                        return callbackOuter(null, DataModel);
 
-        //   var query = "MATCH (n) OPTIONAL MATCH(n)-[r]-(m) "
-        var query = "MATCH(n)-[r]-(m) "
-            + where
-            + " RETURN distinct labels(n) as labels_n, type(r) as type_r,labels(m)[0] as label_m, labels(startNode(r))[0] as label_startNode,count(n) as count_n,count(r) as count_r,count(m) as count_m";
-
-        var payload = {
-            match: query
-        }
-        $.ajax({
-            type: "POST",
-            url: self.neo4jProxyUrl,
-            //data : paramsObj,
-            data: payload,
-            dataType: "json",
-            success: function (data, textStatus, jqXHR) {
-                if (data.length == 0)
-                    callback(null, dataModel);
-
-                //	var data = data.results[0].data;
-
-                for (var i = 0; i < data.length; i++) {
-                    var objNeo = data[i];
-                    var obj = {
-                        labels1: objNeo.labels_n,
-                        label2: objNeo.label_m,
-                        relStartLabel: objNeo.label_startNode,
-                        relType: objNeo.type_r,
-                        count1: objNeo.count_n,
-                        count2: objNeo.count_m,
-                    }
-
-                    if (obj.relType) {
-                        if (!dataModel.labelsRelations[obj.label1])
-                            dataModel.labelsRelations[obj.label1] = [];
-                        dataModel.labelsRelations[obj.label1].push(obj.relType);
-                        if (!dataModel.labelsRelations[obj.label2])
-                            dataModel.labelsRelations[obj.label2] = [];
-                        dataModel.labelsRelations[obj.label2].push(obj.relType);
-
-
-                        if (obj.labels1) {
-                            obj.label1 = obj.labels1[0];
-                            if (obj.label1 == obj.relStartLabel)
-                                obj.direction = "normal";
-                            else
-                                obj.direction = "inverse";
+                    for (var i = 0; i < data.length; i++) {
+                        var objNeo = data[i];
+                        var obj = {
+                            labels1: objNeo.labels_n,
+                            label2: objNeo.label_m,
+                            relStartLabel: objNeo.label_startNode,
+                            relType: objNeo.type_r,
+                            count1: objNeo.count_n,
+                            count2: objNeo.count_m,
                         }
 
-                        if (!dataModel.relations[obj.label1])
-                            dataModel.relations[obj.label1] = [];
-                        dataModel.relations[obj.label1].push(obj);
-
-                        if (!dataModel.allRelations[obj.relType])
-                            dataModel.allRelations[obj.relType] = [];
-
-
-                        dataModel.allRelations[obj.relType].push({
-                            startLabel: obj.label1,
-                            endLabel: obj.label2,
-                            direction: obj.direction
-                        });
-                        if (dataModel.allRelationsArray.indexOf(obj.relType) < 0)
-                            dataModel.allRelationsArray.push(obj.relType);
-                    }
-                }
-                // fields
-                query = "MATCH (n) " + where
-                    + " return distinct labels(n) as labels_n,keys(n) as keys_n,count(n) as count_n";
-                //  + " return distinct labels(n)[0] as label_n,keys(n) as keys_n,count(n) as count_n";
-
-                payload = {match: query};
+                        if (obj.relType) {
+                            if (!DataModel.labelsRelations[obj.label1])
+                                DataModel.labelsRelations[obj.label1] = [];
+                            DataModel.labelsRelations[obj.label1].push(obj.relType);
+                            if (!DataModel.labelsRelations[obj.label2])
+                                DataModel.labelsRelations[obj.label2] = [];
+                            DataModel.labelsRelations[obj.label2].push(obj.relType);
 
 
-                // console.log("QUERY----" + JSON.stringify(payload));
-                $.ajax({
-                    type: "POST",
-                    url: self.neo4jProxyUrl,
-                    data: payload,
-                    dataType: "json",
-                    success: function (data, textStatus, jqXHR) {
-                        for (var i = 0; i < data.length; i++) {
-
-                            var labels = data[i].labels_n;
-
-                            for (var k = 0; k < labels.length; k++) {
-                                var label = labels[k];
-                                if (dataModel.allLabels.indexOf(label) < 0)
-                                    dataModel.allLabels.push(label);
-                                var fields = data[i].keys_n;
-                                if (!dataModel.labels[label])
-                                    dataModel.labels[label] = [];
-
-                                for (var j = 0; j < fields.length; j++) {
-                                    if (dataModel.allProperties
-                                            .indexOf(fields[j]) < 0)
-                                        dataModel.allProperties
-                                            .push(fields[j]);
-                                    if (dataModel.labels[label]
-                                            .indexOf(fields[j]) < 0) {
-                                        dataModel.labels[label]
-                                            .push(fields[j]);
-                                    }
-
-                                }
+                            if (obj.labels1) {
+                                obj.label1 = obj.labels1[0];
+                                if (obj.label1 == obj.relStartLabel)
+                                    obj.direction = "normal";
+                                else
+                                    obj.direction = "inverse";
                             }
+
+                            if (!DataModel.relations[obj.label1])
+                                DataModel.relations[obj.label1] = [];
+                            DataModel.relations[obj.label1].push(obj);
+
+                            if (!DataModel.allRelations[obj.relType])
+                                DataModel.allRelations[obj.relType] = [];
+
+
+                            DataModel.allRelations[obj.relType].push({
+                                startLabel: obj.label1,
+                                endLabel: obj.label2,
+                                direction: obj.direction
+                            });
+                            if (DataModel.allRelationsArray.indexOf(obj.relType) < 0)
+                                DataModel.allRelationsArray.push(obj.relType);
                         }
-
-                        dataModel.allProperties.sort();
-                        dataModel.allLabels.sort();
-
-                        /*  if (dataModel.allProperties.indexOf("subGraph") < 0) {
-                              var queryParamsSubGraph = "";
-                              if (queryParams.subGraph)
-                                  queryParamsSubGraph = queryParams.subGraph;
-                              $("#dialog").dialog("option", "title", "result");
-                              var str = "Souslesens needs a property called subGraph on each node to allow different subsets in the same Neo4j database";
-                              str += "<br>set subGraph property <input id='subGraphName' value='" + queryParamsSubGraph + "'><br><button onclick='dataModel.generateSubGraphPropertyOnAllNodes()'>Apply and continue</button> ";
-                              str += "<br><button onclick='window.close();  $(\"#dialog\").dialog(\"close\")')>Quit souslesens</button>"
-                              $("#dialog").html(str);
-                              $("#dialog").dialog("open");//.position({my: 'center', at: 'center', of: '#tabs-analyzePanel'});
-
-
-                          }*/
-
-
-                        //relation Properties
-                        query = " match(n)-[r]-(m)" + where + " return distinct type(r)as relType,labels(n)[0] as startLabel,labels(m)[0] as endLabel,  keys(r) as relProperties"
-                        payload = {match: query};
-
-
-                        // console.log("QUERY----" + JSON.stringify(payload));
-                        $.ajax({
-                            type: "POST",
-                            url: self.neo4jProxyUrl,
-                            data: payload,
-                            dataType: "json",
-                            success: function (data, textStatus, jqXHR) {
-                                for (var i = 0; i < data.length; i++) {
-
-                                    var relPropsObj = data[i];
-                                    var relationObjs = dataModel.allRelations[relPropsObj.relType];
-                                    for (var j = 0; j < relationObjs.length; j++) {
-                                        var relationObj = relationObjs[j];
-
-                                        if (relationObj && relationObj.direction == "normal" && relationObj.startLabel == relPropsObj.startLabel && relationObj.endLabel == relPropsObj.endLabel) {
-                                            dataModel.allRelations[relPropsObj.relType][j].properties = relPropsObj.relProperties;
-                                        }
-                                        if (relationObj && relationObj.direction == "inverse" && relationObj.endLabel == relPropsObj.startLabel && relationObj.startLabel == relPropsObj.endLabel) {
-                                            dataModel.allRelations[relPropsObj.relType][j].properties = relPropsObj.relProperties;
-                                        }
-                                    }
-
-
-                                }
-                                callback(null, dataModel);
-                            }
-                            /*    // get rel properties
-                                query = "match (n)-[r]-() where n.subGraph=\"" + subGraph + "\" return distinct type(r) relType, keys(r) as keys"
-                                payload = {match: query};
-                                $.ajax({
-                                    type: "POST",
-                                    url: self.neo4jProxyUrl,
-                                    data: payload,
-                                    dataType: "json",
-                                    success: function (data, textStatus, jqXHR) {
-                                        data.forEach(function (line) {
-                                            var x=dataModel.allRelations
-                                        })
-
-                                        callback(null, dataModel);
-                                    }
-                                    ,
-                                    error: function (xhr, err, msg) {
-                                        callback(null);
-                                        toutlesensController.onErrorInfo(xhr)
-                                    }
-                                })
-                            }*/
-
-
-                            ,
-                            error: function (xhr, err, msg) {
-                                callback(null);
-                                toutlesensController.onErrorInfo(xhr)
-                            }
-                        })
-
-
                     }
-                    ,
-                    error: function (xhr, err, msg) {
-                        callback(null);
-                        toutlesensController.onErrorInfo(xhr)
-                    }
+                    callback();
+
 
                 })
-
             },
-            error: function (xhr, err, msg) {
-                toutlesensController.onErrorInfo(xhr)
+            function (callback) {
+                var where = "";
+                if (subGraph && subGraph != "") ;
+                var sql = "MATCH (n) " + where
+                    + " return distinct labels(n) as labels_n,keys(n) as keys_n,count(n) as count_n";
+
+                Cypher.executeCypher(sql, function (err, data) {
+                    if (err)
+                        return callback(err);
+                    if (data.length == 0)
+                        return callbackOuter(null, DataModel);
+
+
+                    for (var i = 0; i < data.length; i++) {
+
+                        var labels = data[i].labels_n;
+
+                        for (var k = 0; k < labels.length; k++) {
+                            var label = labels[k];
+                            if (DataModel.allLabels.indexOf(label) < 0)
+                                DataModel.allLabels.push(label);
+                            var fields = data[i].keys_n;
+                            if (!DataModel.labels[label])
+                                DataModel.labels[label] = [];
+
+                            for (var j = 0; j < fields.length; j++) {
+                                if (DataModel.allProperties
+                                        .indexOf(fields[j]) < 0)
+                                    DataModel.allProperties
+                                        .push(fields[j]);
+                                if (DataModel.labels[label]
+                                        .indexOf(fields[j]) < 0) {
+                                    DataModel.labels[label]
+                                        .push(fields[j]);
+                                }
+
+                            }
+                        }
+                    }
+
+                    DataModel.allProperties.sort();
+                    DataModel.allLabels.sort();
+                    callback();
+                })
             }
+            ,
+            function (callback) {
+                var where = "";
+                if (subGraph && subGraph != "") ;
+                var sql = " match(n)-[r]-(m)" + where + " return distinct type(r)as relType,labels(n)[0] as startLabel,labels(m)[0] as endLabel,  keys(r) as relProperties"
+                Cypher.executeCypher(sql, function (err, data) {
 
-        });
+                    if (err)
+                        return callback(err);
+                    if (data.length == 0)
+                        return callbackOuter(null, DataModel);
 
+                    for (var i = 0; i < data.length; i++) {
+
+                        var relPropsObj = data[i];
+                        var relationObjs = DataModel.allRelations[relPropsObj.relType];
+                        for (var j = 0; j < relationObjs.length; j++) {
+                            var relationObj = relationObjs[j];
+
+                            if (relationObj && relationObj.direction == "normal" && relationObj.startLabel == relPropsObj.startLabel && relationObj.endLabel == relPropsObj.endLabel) {
+                                DataModel.allRelations[relPropsObj.relType][j].properties = relPropsObj.relProperties;
+                            }
+                            if (relationObj && relationObj.direction == "inverse" && relationObj.endLabel == relPropsObj.startLabel && relationObj.startLabel == relPropsObj.endLabel) {
+                                DataModel.allRelations[relPropsObj.relType][j].properties = relPropsObj.relProperties;
+                            }
+                        }
+
+
+                    }
+
+
+                    callback();
+                })
+            }
+        ], function (err) {
+            if (err)
+                return callbackOuter(err);
+            return callbackOuter(null, DataModel);
+
+        })
     }
+      
 
     self.generateSubGraphPropertyOnAllNodes = function () {
         var name = $("#subGraphName").val();
