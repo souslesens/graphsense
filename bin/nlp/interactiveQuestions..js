9b33105741cd83eb519ab441159d4daa479819c2
Claude.fauconnet <claude.fauconnet@neuf.fr>
2019-06-30 20:15:11 +0200

    corrections

--- bin/nlp/interactiveQuestions..js
+++ bin/nlp/interactiveQuestions..js
@@ -10,10 +10,10 @@ var elasticUrl = "http://localhost:9200/thesaurus_ctg/_search";
 var InteractiveQuestions={
 
     getSentenceEntityProposals:function(sentence,callback){
-
+        sentence=sentence.toLowerCase()
         var words=sentence.split(" ");
 
-        var proposals=[];
+        var proposals={entities:[],words:[]};
         async.eachSeries(words,function(word,callbackEach){
 
             var  payload = {
@@ -51,8 +51,11 @@ var InteractiveQuestions={
                             var xx = res.body
                             res.body.hits.hits.forEach(function (hit) {
                                 var obj = {id: hit._iD, score: hit._score,data:hit._source};
-                                proposals.push(obj)
+                                proposals.entities.push(obj)
                             })
+                        }else{
+                            if(InteractiveQuestions.englishStopWords.indexOf(word)<0)
+                            proposals.words.push(word)
                         }
                         callbackEach()
                     }
@@ -69,7 +72,135 @@ var InteractiveQuestions={
 
             })
 
-    }
+    },
+
+    englishStopWords:["i",
+        "me",
+        "my",
+        "myself",
+        "we",
+        "our",
+        "ours",
+        "ourselves",
+        "you",
+        "your",
+        "yours",
+        "yourself",
+        "yourselves",
+        "he",
+        "him",
+        "his",
+        "himself",
+        "she",
+        "her",
+        "hers",
+        "herself",
+        "it",
+        "its",
+        "itself",
+        "they",
+        "them",
+        "their",
+        "theirs",
+        "themselves",
+        "what",
+        "which",
+        "who",
+        "whom",
+        "this",
+        "that",
+        "these",
+        "those",
+        "am",
+        "is",
+        "are",
+        "was",
+        "were",
+        "be",
+        "been",
+        "being",
+        "have",
+        "has",
+        "had",
+        "having",
+        "do",
+        "does",
+        "did",
+        "doing",
+        "a",
+        "an",
+        "the",
+        "and",
+        "but",
+        "if",
+        "or",
+        "because",
+        "as",
+        "until",
+        "while",
+        "of",
+        "at",
+        "by",
+        "for",
+        "with",
+        "about",
+        "against",
+        "between",
+        "into",
+        "through",
+        "during",
+        "before",
+        "after",
+        "above",
+        "below",
+        "to",
+        "from",
+        "up",
+        "down",
+        "in",
+        "out",
+        "on",
+        "off",
+        "over",
+        "under",
+        "again",
+        "further",
+        "then",
+        "once",
+        "here",
+        "there",
+        "when",
+        "where",
+        "why",
+        "how",
+        "all",
+        "any",
+        "both",
+        "each",
+        "few",
+        "more",
+        "most",
+        "other",
+        "some",
+        "such",
+        "no",
+        "nor",
+        "not",
+        "only",
+        "own",
+        "same",
+        "so",
+        "than",
+        "too",
+        "very",
+        "s",
+        "t",
+        "can",
+        "will",
+        "just",
+        "don",
+        "should",
+        "now"]
 
 
 
